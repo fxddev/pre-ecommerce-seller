abd76ffc0e07f09bcb1909146b614ce4c2944e9c
dyahbalzammo <108322066+dyahbalzammo@users.noreply.github.com>
2023-01-07 09:40:38 +0000

    up

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from "react-router-dom";
 
 import '../assets/products.css'
+import '../assets/switch.css'
 
 import PocketBase from 'pocketbase';
 
@@ -12,7 +13,7 @@ function Products() {
     const navigate = useNavigate();
 
     const is_valid = pb.authStore.isValid
-    
+
     let id_user
 
     useEffect(() => {
@@ -33,7 +34,58 @@ function Products() {
         });
         console.log(records);
 
-        setProducts(records)
+        // console.log("id_user aka idPenjual");
+        // console.log(id_user);
+
+        let array = []
+        for (let i = 0; i < records.length; i++) {
+            if (records[i].id_penjual == id_user) {
+                array.push(records[i])
+            }
+        }
+
+        setProducts(array)
+    }
+
+    async function handleSwitch(idProduct) {
+        console.log(idProduct);
+
+        let array = []
+        for (let i = 0; i < products.length; i++) {
+            if (products[i].id == idProduct) {
+                console.log(products[i]);
+
+                const new_status = !products[i].status
+
+                const obj = {
+                    "id": products[i].id,
+                    "nama": products[i].nama,
+                    "img": products[i].img,
+                    "harga": products[i].harga,
+                    "deskripsi": products[i].deskripsi,
+                    "kondisi": products[i].kondisi,
+                    "minimum_pembelian": products[i].minimum_pembelian,
+                    "berat": products[i].berat,
+                    "stock": products[i].stock,
+                    "id_penjual": products[i].id_penjual,
+                    "status": new_status,
+                    "created": products[i].created,
+                    "updated": products[i].updated,
+                    "collectionId": products[i].collectionId,
+                    "collectionName": products[i].collectionName
+                }
+                array.push(obj)
+
+                const data = {
+                    "status": new_status
+                };
+                
+                const record = await pb.collection('products').update(idProduct, data);
+            } else {
+                array.push(products[i])
+            }            
+        }
+        setProducts(array)
     }
 
     return (
@@ -56,9 +108,18 @@ function Products() {
                             <td>{p.nama}</td>
                             <td>{p.harga}</td>
                             <td>{p.stock}</td>
-                            <td>{p.status}</td>
                             <td>
-                                <button>Edit </button>
+                                <label class="switch">
+                                    {p.status ?
+                                        <input type="checkbox" checked onClick={() => handleSwitch(p.id)} />
+                                        :
+                                        <input type="checkbox" onClick={() => handleSwitch(p.id)} />
+                                    }
+                                    <span class="slider round"></span>
+                                </label>
+                            </td>
+                            <td>
+                                <button>Edit</button>
                                 <button>Hapus</button>
                             </td>
                         </tr>
