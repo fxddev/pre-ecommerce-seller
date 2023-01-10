4bea0662a5b71233c26d6a13294788568510fd2a
dyahbalzammo <108322066+dyahbalzammo@users.noreply.github.com>
2023-01-10 07:55:13 +0000

    up

--- src/pages/Pesanan.jsx
+++ src/pages/Pesanan.jsx
@@ -27,16 +27,56 @@ function Pesanan() {
         // getCartsById()
     }, []);
 
+    const [allPsn, setAllPsn] = useState([]);
     async function getAllPesanan() {
         const records = await pb.collection('pesanan').getFullList(200 /* batch size */, {
             sort: '-created',
         });
         
         console.log(records);
+        setAllPsn(records)
+
+        for (let i = 0; i < records.length; i++) {
+            if (records[i].midtrans_response.transaction_status) {
+                
+            }            
+        }
+    }
+    
+    const [tampilan, setTampilan] = useState("psnbaru");
+    const [psnBaru, setPsnBaru] = useState([]);
+    const [siapKrm, setSiapKrm] = useState([]);
+    const [otw, setOtw] = useState([]);
+    function handleTampilan(val) {
+        setTampilan(val)
     }
 
     return (
-        <p>Ini pes</p>
+        <div>
+            <div>
+                <button onClick={() => handleTampilan("psnbaru")}>Pesanan Baru</button>
+                <button onClick={() => handleTampilan("siapkrm")}>Siap dikirim</button>
+                <button onClick={() => handleTampilan("otw")}>Dalam pengiriman</button>
+            </div>
+
+            {tampilan == "psnbaru" && 
+                <>
+                <p>psnbaru</p>
+                </>
+            }
+
+            {tampilan == "siapkrm" && 
+                <>
+                <p>siapkrm</p>
+                </>
+            }
+
+            {tampilan == "otw" && 
+                <>
+                <p>otw</p>
+                </>
+            }
+        </div>
     )
 }
 
